c1f31e976c7b46d912a807f1ac3260ca3d806589
Leona-Wang <leonawang920913@gmail.com>
2023-12-21 00:16:35 +0800

    2312210016

--- DSFinal_8_new/src/main/java/BooksSearch.java
+++ DSFinal_8_new/src/main/java/BooksSearch.java
@@ -30,75 +30,71 @@ public class BooksSearch {
 	
 	public void search() {
 		int timeOut=20000;
-   	 int waitForBackgroundJavaScript = 20000;
-   	 
-   	 final WebClient webClient=new WebClient(BrowserVersion.CHROME);
-   	 webClient.getOptions().setCssEnabled(false);
-   	 webClient.getOptions().setJavaScriptEnabled(true);
-   	 
-   	 webClient.getOptions().setThrowExceptionOnScriptError(false);
-   	 webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);
-   	 webClient.getOptions().setActiveXNative(false);
-   	 webClient.getOptions().setTimeout(timeOut);
-   	 
-   	 webClient.setJavaScriptErrorListener(new DummyJavascriptErrorListener());
-   	 webClient.setIncorrectnessListener(new DummyIncorrectnessListener());
-   	 
-   	 webClient.getOptions().setDownloadImages(false);
-   	 
-   	 webClient.setAjaxController(new NicelyResynchronizingAjaxController());
-   	 
-   	 
-   	 webClient.setJavaScriptTimeout(timeOut);
-   	 
-   	 HtmlPage page = null;
-   	 String pageXml="";
-   	 
-   	 try {
-   		page=webClient.getPage("https://search.books.com.tw/search/query/key/%E6%8B%96%E9%9E%8B/cat/all");
-      	 webClient.waitForBackgroundJavaScript(waitForBackgroundJavaScript);
-      	 Thread.sleep(10000);
-   		 pageXml=page.asXml();
-   		 Document parse=Jsoup.parse(pageXml);
-   		 
-   		 
-   		 //System.out.println(parse);
-           // Get first table
-           Elements divs = parse.select("div.table-td");
-           
-           /*Elements spans=parse.select("span.price");*/
-           Elements as=parse.select("div.table-td a");
-           
-           // Get td Iterator
-           System.out.println(divs.size());
-           System.out.println(as.size());
-           //System.out.println(spans.size());
-          
-          for (int i=0;i<10;i++) {
-         	  
-   	       	  String a=divs.get(i).text();
-   	       	  //String b=spans.get(i).text();
-   	       	  String citeUrl = as.get(2*i).attr("href");
-   	       	  
-   	       	  System.out.println(a);
-   	       	  if (citeUrl.equals("")) {
-   	       		  System.out.println("no");
-   	       	  }
-   	       	  else {
-   	       		  System.out.println("https:"+citeUrl);
-   	       		  
-   	       	  }
-   	       	System.out.println("----------------------------\n\n"); 
-         	  
-           }
-      	 
-      	 
-      	 webClient.close();
-   	
-   	 }catch(Exception e) {
-   		 
-   	 }
-   	 
+		int waitForBackgroundJavaScript = 20000;
+		   	 
+		final WebClient webClient=new WebClient(BrowserVersion.CHROME);
+		webClient.getOptions().setCssEnabled(false);
+		webClient.getOptions().setJavaScriptEnabled(true);
+		   	 
+		webClient.getOptions().setThrowExceptionOnScriptError(false);
+		webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);
+		webClient.getOptions().setActiveXNative(false);
+		webClient.getOptions().setTimeout(timeOut);
+		   	 
+		webClient.setJavaScriptErrorListener(new DummyJavascriptErrorListener());
+		webClient.setIncorrectnessListener(new DummyIncorrectnessListener());
+		   	 
+		webClient.getOptions().setDownloadImages(false);
+		   	 
+		webClient.setAjaxController(new NicelyResynchronizingAjaxController());
+		   	 
+		   	 
+		webClient.setJavaScriptTimeout(timeOut);
+		   	 
+		HtmlPage page = null;
+		String pageXml="";
+		   	 
+		try {
+			page=webClient.getPage("https://search.books.com.tw/search/query/key/%E6%8B%96%E9%9E%8B/cat/all");
+			webClient.waitForBackgroundJavaScript(waitForBackgroundJavaScript);
+			Thread.sleep(10000);
+			pageXml=page.asXml();
+			Document parse=Jsoup.parse(pageXml);
+			 
+			Elements divs = parse.select("div.table-td");
+			 
+			Elements as=parse.select("div.table-td a");
+			           
+			// Get td Iterator
+			System.out.println(divs.size());
+			System.out.println(as.size());
+			//System.out.println(spans.size());
+			          
+			for (int i=0;i<10;i++) {
+			         	  
+				String a=divs.get(i).text();
+				//String b=spans.get(i).text();
+				String citeUrl = as.get(2*i).attr("href");
+				   	       	  
+				System.out.println(a);
+				if (citeUrl.equals("")) {
+					System.out.println("no");
+				}
+				else {
+					System.out.println("https:"+citeUrl);
+				   	       		  
+				}
+				System.out.println("----------------------------\n\n"); 
+				         	  
+			}
+			      	 
+			      	 
+			webClient.close();
+			   	
+		}catch(Exception e) {
+		   		 
+		}
+		   	 
 	}
 
 }
--- DSFinal_8_new/src/main/java/RakutenSearch.java
+++ DSFinal_8_new/src/main/java/RakutenSearch.java
@@ -31,78 +31,76 @@ public class RakutenSearch {
 	
 	
 	public void search() {
+		
 		int timeOut=20000;
-   	 int waitForBackgroundJavaScript = 20000;
-   	 
-   	 final WebClient webClient=new WebClient(BrowserVersion.CHROME);
-   	 webClient.getOptions().setCssEnabled(false);
-   	 webClient.getOptions().setJavaScriptEnabled(true);
-   	 
-   	 webClient.getOptions().setThrowExceptionOnScriptError(false);
-   	 webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);
-   	 webClient.getOptions().setActiveXNative(false);
-   	 webClient.getOptions().setTimeout(timeOut);
-   	 
-   	 webClient.setJavaScriptErrorListener(new DummyJavascriptErrorListener());
-   	 webClient.setIncorrectnessListener(new DummyIncorrectnessListener());
-   	 
-   	 webClient.getOptions().setDownloadImages(false);
-   	 
-   	 webClient.setAjaxController(new NicelyResynchronizingAjaxController());
-   	 
-   	 
-   	 webClient.setJavaScriptTimeout(timeOut);
-   	 
-   	 HtmlPage page = null;
-   	 String pageXml="";
-   	 
-   	 try {
-   		 page=webClient.getPage("https://www.rakuten.com.tw/search/%E6%B2%99%E7%99%BC/");
-      	 webClient.waitForBackgroundJavaScript(waitForBackgroundJavaScript);
-      	 Thread.sleep(10000);
-   		 pageXml=page.asXml();
-   	 }catch(Exception e) {
-   		 
-   	 }
-   	 
-   	 
-		 //Document parse=Jsoup.parse(pageXml);
-		 
-		 int nameIdx=0;
-		 int urlIdx=0;
-		 int priceIdx=0;
-		 int minIdx=0;
-		 int maxIdx=0;
-		 int itemListPriceIdx=0;
-		 
-		 String name="";
-		 String url="";
-		 String min="";
-		 String max="";
-		 
-		 for (int i=0;i<10;i++) {
-			 
-			 nameIdx=pageXml.indexOf("itemName");
-			 urlIdx=pageXml.indexOf("itemUrl");
-			 priceIdx=pageXml.indexOf("itemPrice");
-			 minIdx=pageXml.indexOf("\"min\"");
-			 maxIdx=pageXml.indexOf("\"max\"");
-			 itemListPriceIdx=pageXml.indexOf("\"itemListPrice\"");
-			 
-			 name=pageXml.substring(nameIdx+11, urlIdx-3);
-			 System.out.println(name);
-			 url=pageXml.substring(urlIdx+10, priceIdx-3);
-			 System.out.println(url);
-			 min=pageXml.substring(minIdx+6, maxIdx-1);
-			 System.out.println(min);
-			 max=pageXml.substring(maxIdx+6, itemListPriceIdx-2);
-			 System.out.println(max+"\n------------------------");
-			 
-			 pageXml=pageXml.substring(itemListPriceIdx+1);
-			 nameIdx=pageXml.indexOf("itemName");
-			 pageXml=pageXml.substring(nameIdx-1);
-		 }
-		 
-		 webClient.close();
+		int waitForBackgroundJavaScript = 20000;
+		   	 
+		final WebClient webClient=new WebClient(BrowserVersion.CHROME);
+		webClient.getOptions().setCssEnabled(false);
+		webClient.getOptions().setJavaScriptEnabled(true);
+		   	 
+		webClient.getOptions().setThrowExceptionOnScriptError(false);
+		webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);
+		webClient.getOptions().setActiveXNative(false);
+		webClient.getOptions().setTimeout(timeOut);
+		   	 
+		webClient.setJavaScriptErrorListener(new DummyJavascriptErrorListener());
+		webClient.setIncorrectnessListener(new DummyIncorrectnessListener());
+		   	 
+		webClient.getOptions().setDownloadImages(false);
+		   	 
+		webClient.setAjaxController(new NicelyResynchronizingAjaxController());
+		   	 
+		   	 
+		webClient.setJavaScriptTimeout(timeOut);
+		   	 
+		HtmlPage page = null;
+		String pageXml="";
+		   	 
+		try {
+			page=webClient.getPage("https://www.rakuten.com.tw/search/%E6%B2%99%E7%99%BC/");
+			webClient.waitForBackgroundJavaScript(waitForBackgroundJavaScript);
+			Thread.sleep(10000);
+			pageXml=page.asXml();
+		}catch(Exception e) {
+		   		 
+		}
+
+		int nameIdx=0;
+		int urlIdx=0;
+		int priceIdx=0;
+		int minIdx=0;
+		int maxIdx=0;
+		int itemListPriceIdx=0;
+				 
+		String name="";
+		String url="";
+		String min="";
+		String max="";
+				 
+		for (int i=0;i<10;i++) {
+					 
+			nameIdx=pageXml.indexOf("itemName");
+			urlIdx=pageXml.indexOf("itemUrl");
+			priceIdx=pageXml.indexOf("itemPrice");
+			minIdx=pageXml.indexOf("\"min\"");
+			maxIdx=pageXml.indexOf("\"max\"");
+			itemListPriceIdx=pageXml.indexOf("\"itemListPrice\"");
+						 
+			name=pageXml.substring(nameIdx+11, urlIdx-3);
+			System.out.println(name);
+			url=pageXml.substring(urlIdx+10, priceIdx-3);
+			System.out.println(url);
+			min=pageXml.substring(minIdx+6, maxIdx-1);
+			System.out.println(min);
+			max=pageXml.substring(maxIdx+6, itemListPriceIdx-2);
+			System.out.println(max+"\n------------------------");
+						 
+			pageXml=pageXml.substring(itemListPriceIdx+1);
+			nameIdx=pageXml.indexOf("itemName");
+			pageXml=pageXml.substring(nameIdx-1);
+		}
+				 
+		webClient.close();
 	}
 }
